4e963be000acf31e809a88037fe8fbe705fc1cee
Pedro Artur <71746213+Pedro-Artur-RF@users.noreply.github.com>
2025-02-27 04:58:10 -0300

    Security update & QOL update
    Added a settings folder for future commands configuration.
    Changed main.py to use external settings file for the bot set up values

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-import discord
+import discord, os, json
 from discord.ext import commands
 
-botToken = ''
+# Carrega as configurações do arquivo JSON
+with open('./config/MainConfig.json', 'r') as f:
+    config = json.load(f)
 
+# Obtém o token do bot e o prefixo de comando a partir do arquivo de configuração
+botToken = config['token']
+prefix = config['prefix']
+
+# Define as permissões necessárias para o bot
 intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
-intents.guild_messages = True
-intents.messages = True
+intents.members = True  # Permite acessar eventos de membros
+intents.message_content = True  # Permite acessar o conteúdo das mensagens
+intents.guild_messages = True  # Permite acessar mensagens do servidor
+intents.messages = True  # Permite acessar mensagens em geral
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+# Cria o bot com o prefixo de comando e as permissões definidas
+bot = commands.Bot(command_prefix=prefix, intents=intents)
 
+# Evento acionado quando o bot estiver pronto
 @bot.event
 async def on_ready():
     print(f'{bot.user} logged 🔵🔵🔵')
 
+# Evento de configuração do bot, carregando as extensões (comandos e eventos)
 @bot.event
 async def setup_hook():
     await bot.load_extension('commands.oioi')
@@ -23,7 +33,9 @@ async def setup_hook():
     await bot.load_extension('commands.channels')
     await bot.load_extension('commands.servers')
     await bot.load_extension('commands.log')
+    await bot.load_extension('commands.init')
     await bot.load_extension('events.autodado')
 
+# Inicia o bot com o token carregado do arquivo de configuração
 if __name__ == "__main__":    
     bot.run(botToken)
